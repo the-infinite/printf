a2e018f92d7635f865e71e865e5cbdab7e18794d
the-infinite <toby.johnson.precious@gmail.com>
2022-10-18 00:20:02 +0100

    Fixed: Added support for hexadecimal numbers and refactored carefully so work should be easier for you.

--- parser.c
+++ parser.c
@@ -1,5 +1,5 @@
-
 #include "parser.h"
+
 #include "printer.h"
 
 /**
@@ -40,7 +40,7 @@ static void __case_percent(int *printed)
 }
 
 /**
- * __case_number - Handles the case where t print a number.
+ * __case_number - Handles the case where to print a number.
  * @args: The variadic list to eat the number from.
  * @printed: A reference to an integer that's counting the number
  * of printed characters.
@@ -56,7 +56,7 @@ static void __case_number(va_list args, int *printed)
 }
 
 /**
- * __case_ui - Handles the case where t print a number.
+ * __case_ui - Handles the case where to print an unsigned number.
  * @args: The variadic list to eat the number from.
  * @printed: A reference to an integer that's counting the number
  * of printed characters.
@@ -68,6 +68,64 @@ static void __case_ui(va_list args, int *printed)
 	{
 		/* Tell the user we failed to write. */
 		exit(-1);
+	}
+}
+
+/**
+ * __case_hex - Handles the case where to print a hexadecimal number.
+ * @args: The variadic list to eat the number from.
+ * @printed: A reference to an integer that's counting the number
+ * @upper: Are we doing this in uppercase?
+ * of printed characters.
+ * Return: Nothing.
+*/
+static void __case_hex(va_list args, int *printed, char upper)
+{
+	if (!__write_hex(args, printed, upper))
+	{
+		exit(-1);
+	}
+}
+
+/**
+ * __do_switch - Performs the necessary case control for the parsing of format
+ * specifiers.
+ * @format: The template string.
+ * @current: A pointer to the current integer.
+ * @printed: The number of characters we have printed so far.
+ * @args: The list of variable arguments.
+ * Return: Nothing.
+*/
+static void __do_switch(const char *format, int *current, int *printed,
+va_list args) {
+	switch (format[*current])
+	{
+		case '%': /* have we found a percent literal? */ {
+			__case_percent(printed);
+		}	break;
+
+		case 'X': case 'x': {
+			__case_hex(args, printed, format[*current] == 'X');
+		}	break;
+
+		case 'P': case 'p': {
+			if (!__write_pointer(args, printed, format[*current] == 'P'))
+			{
+				exit(-1);
+			}
+		}	break;
+
+		case 'i': case 'd': {
+			__case_number(args, printed);
+		}	break;
+
+		case 'u': {
+			__case_ui(args, printed);
+		}	break;
+
+		default: { /* If it is unknown, just print as is. */
+			__case_default(format, *current, printed);
+		}	break;
 	}
 }
 
@@ -90,26 +148,7 @@ int __printf_parseString(const char *format, va_list args)
 		if (format[current] == '%')
 		{
 			current++; /* Move away from the percentage sign into the f_type */
-
-			switch (format[current])
-			{
-				case '%': /* have we found a format specifier? */ {
-					__case_percent(&printed);
-				}	break;
-
-				case 'i':
-				case 'd': {
-					__case_number(args, &printed);
-				}	break;
-
-				case 'u': {
-					__case_ui(args, &printed);
-				}	break;
-
-				default: { /* If it is unknown, just print as is. */
-					__case_default(format, current, &printed);
-				}	break;
-			}
+			__do_switch(format, &current, &printed, args);
 		}
 
 		else
---
+++ printer.hex.c
@@ -0,0 +1,137 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "printer.h"
+
+
+/**
+* __util_reverse - A utility function to reverse a string.
+* @str: The string to reverse.
+* @size: The size of the string to reverse.
+* Return: @str reversed and NULL if str is an empty or NULL string.
+*/
+static char *__util_reverse(char *str, size_t size)
+{
+	char *reversed;
+
+	unsigned int i;
+
+	if (str == NULL || size == 0)
+	{
+		return (NULL);
+	}
+
+	/* Pad with an extra slot for the NULL terminator. */
+	reversed = malloc(sizeof(char) * (size + 1));
+
+	for (i = 0; i < size / 2; i++)
+	{
+		char tmp = str[i];
+
+		reversed[i] = str[size - i - 1];
+		reversed[size - i - 1] = tmp;
+	}
+
+	return (reversed);
+}
+
+/**
+* __write_hex - This is a function that prints a hexadecimal number to the
+* format specified.
+* @args: The variable arguments list we expect this number to come from.
+* @printed: A pointer to the number of characters we have printed so far
+* @upper: Should this be printed in uppercase?
+* Return: True if printed and false otherwise.
+*/
+int __write_hex(va_list args, int *printed, char upper)
+{
+	unsigned int value, pos = 0, i;
+	char *digits;
+	char *ret, *final;
+
+	if (upper)
+		digits = "0123456789ABCDEF";
+	else
+		digits = "0123456789abcdef";
+
+	value = va_arg(args, unsigned int);
+	ret = malloc(sizeof(char) * 32);
+
+	while (value > 0)
+	{
+		unsigned int digit;
+
+		digit = value % 16; /* What is the remainder when we divide by 16? */
+		ret[pos++] = digits[digit]; /* Look for the digit in B-16 encode*/
+
+		/*Divide the current value of value by 16 to shift to the next digit*/
+		value /= 16;
+	}
+
+	ret[pos] = 0; /* Append a null character to the tail of the string. */
+	final = __util_reverse(ret, (size_t)pos);
+
+	/* Deallocate this piece of memory since it is now unused */
+	free(ret);
+
+	for (i = 0; i < pos; i++)
+	{
+		if (!__write_char(final[i], printed))
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
+
+/**
+* __write_pointer - This is a function that prints a hexadecimal number to the
+* format specified.
+* @args: The variable arguments list we expect this pointer to come from.
+* @printed: A pointer to the number of characters we have printed so far
+* @upper: Should this be printed in uppercase?
+* Return: True if printed and false otherwise.
+*/
+int __write_pointer(va_list args, int *printed, char upper)
+{
+	unsigned long int value, pos = 0, i;
+	char *digits;
+	char *ret, *final;
+
+	if (upper)
+		digits = "0123456789ABCDEF";
+	else
+		digits = "0123456789abcdef";
+
+	value = va_arg(args, unsigned long int);
+	ret = malloc(sizeof(char) * 32);
+
+	while (value > 0)
+	{
+		unsigned int digit;
+
+		digit = value % 16; /* What is the remainder when we divide by 16? */
+		ret[pos++] = digits[digit]; /* Look for the digit in B-16 encode*/
+
+		/*Divide the current value of value by 16 to shift to the next digit*/
+		value /= 16;
+	}
+
+	ret[pos] = 0; /* Append a null character to the tail of the string. */
+	final = __util_reverse(ret, (size_t)pos);
+
+	free(ret);
+
+	if (!__write_char('0', printed))
+		return (0);
+
+	if (!__write_char('x', printed))
+		return (0);
+
+	for (i = 0; i < pos; i++)
+		if (!__write_char(final[i], printed))
+			return (0);
+
+	return (1);
+}
